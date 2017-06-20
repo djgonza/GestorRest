0aee655082f5382d2cf374b2f95496305e11b5d6
djgonza <davidjimenezgonzalez@MacBook-Pro-de-David.local>
2017-06-20 11:41:39 +0200

    Update

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use('/library', require('./controllers/library.controller'));
 
 // start server
 var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+console.log('server', port);
 var server = app.listen(port, function () {
 	console.log('Server listening on port ' + port);
 });
